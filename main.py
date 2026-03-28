8062ff226960e8a52a74f93481f0c2ada05f0b5c
nerdpremier <touchpeera@gmail.com>
2026-03-28 23:19:57 +0700

    engine

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ app = FastAPI(title="Risk Engine", version="5.0-rawscore-pivot-autoclick")
 MODEL_PATH = os.getenv("MODEL_PATH", "isolation_forest_model.pkl")
 API_SECRET = os.getenv("RISK_API_SECRET", "change-me")
 
-PIVOT_RATIO = float(os.getenv("PIVOT_RATIO", "0.15"))
-ANOMALY_GAMMA = float(os.getenv("ANOMALY_GAMMA", "2.5"))
+PIVOT_RATIO = float(os.getenv("PIVOT_RATIO", "0.30"))
+ANOMALY_GAMMA = float(os.getenv("ANOMALY_GAMMA", "1.0"))
 
 EPS = 1e-12
 
